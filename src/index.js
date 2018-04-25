0860ecd97588c71bb64c0f3783c41f2f614503da
yongheng <1074827582@qq.com>
2018-04-25 18:47:48 +0800

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,167 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Search from './page/search/searchTest'
-import registerServiceWorker from './registerServiceWorker';
-
-let data = [
-    { category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football" },
-    { category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball" },
-    { category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball" },
-    { category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch" },
-    { category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5" },
-    { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
-];
+// import { Router, Route, Switch } from 'react-router'
+// import { HashRouter } from 'react-router-dom'
+// import './index.css';
+// import App from './App';
+// import Search from './page/search/searchTest'
+// import registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render(<Search product={data} />, document.getElementById('root'));
-registerServiceWorker();
-
-// class ProductCategoryRow extends React.Component {
-//     render() {
-//         return (<tr><th colSpan="2">{this.props.category}</th></tr>);
-//     }
-// }
-
-// class ProductRow extends React.Component {
-//     render() {
-//         var name = this.props.product.stocked ?
-//             this.props.product.name :
-//             <span style={{ color: 'red' }}>
-//                 {this.props.product.name}
-//             </span>;
-//         return (
-//             <tr>
-//                 <td>{name}</td>
-//                 <td>{this.props.product.price}</td>
-//             </tr>
-//         );
-//     }
-// }
-
-// class ProductTable extends React.Component {
-//     render() {
-//         var rows = [];
-//         var lastCategory = null;
-//         console.log(this.props.inStockOnly)
-//         this.props.products.forEach((product) => {
-//             if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
-//                 return;
-//             }
-//             if (product.category !== lastCategory) {
-//                 rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-//             }
-//             rows.push(<ProductRow product={product} key={product.name} />);
-//             lastCategory = product.category;
-//         });
-//         console.log(rows)
-//         return (
-//             <table>
-//                 <thead>
-//                     <tr>
-//                         <th>Name</th>
-//                         <th>Price</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>{rows}</tbody>
-//             </table>
-//         );
-//     }
-// }
-
-// class SearchBar extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
-//         this.handleInStockInputChange = this.handleInStockInputChange.bind(this);
-//     }
-
-//     handleFilterTextInputChange(e) {
-//         this.props.onFilterTextInput(e.target.value);
-//     }
-
-//     handleInStockInputChange(e) {
-//         this.props.onInStockInput(e.target.checked);
-//     }
-
-//     render() {
-//         return (
-//             <form>
-//                 <input
-//                     type="text"
-//                     placeholder="Search..."
-//                     value={this.props.filterText}
-//                     onChange={this.handleFilterTextInputChange}
-//                 />
-//                 <p>
-//                     <input
-//                         type="checkbox"
-//                         checked={this.props.inStockOnly}
-//                         onChange={this.handleInStockInputChange}
-//                     />
-//                     {' '}
-//                     Only show products in stock
-//           </p>
-//             </form>
-//         );
-//     }
-// }
-
-// class FilterableProductTable extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             filterText: '',
-//             inStockOnly: false
-//         };
-
-//         this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
-//         this.handleInStockInput = this.handleInStockInput.bind(this);
-//     }
-
-//     handleFilterTextInput(filterText) {
-//         this.setState({
-//             filterText: filterText
-//         });
-//     }
-
-//     handleInStockInput(inStockOnly) {
-//         this.setState({
-//             inStockOnly: inStockOnly
-//         })
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <SearchBar
-//                     filterText={this.state.filterText}
-//                     inStockOnly={this.state.inStockOnly}
-//                     onFilterTextInput={this.handleFilterTextInput}
-//                     onInStockInput={this.handleInStockInput}
-//                 />
-//                 <ProductTable
-//                     products={this.props.products}
-//                     filterText={this.state.filterText}
-//                     inStockOnly={this.state.inStockOnly}
-//                 />
-//             </div>
-//         );
-//     }
-// }
-
-
-// var PRODUCTS = [
-//     { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-//     { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-//     { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-//     { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-//     { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-//     { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+// let data = [
+//     { category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football" },
+//     { category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball" },
+//     { category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball" },
+//     { category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch" },
+//     { category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5" },
+//     { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
 // ];
 
-// ReactDOM.render(
-//     <FilterableProductTable products={PRODUCTS} />,
-//     document.getElementById('root')
-// );
+
+// ReactDOM.render((
+// <HashRouter>
+//   <App/>
+// </HashRouter>), document.getElementById('root'));
+// // ReactDOM.render(<Search product={data} />, document.getElementById('root'));
+// registerServiceWorker();
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">首页</Link></li>
+        <li><Link to="/about">关于</Link></li>
+        <li><Link to="/topics">主题列表</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
+
+const Home = () => (
+  <div>
+    <h2>首页</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>关于</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>主题列表</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          使用 React 渲染
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          组件
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          属性 v. 状态
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>请选择一个主题。</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+ReactDOM.render(<BasicExample />, document.getElementById('root'));
